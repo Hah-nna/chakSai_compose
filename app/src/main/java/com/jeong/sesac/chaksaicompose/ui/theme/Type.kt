ceeb4b1de11a55a93692b0f3dd6fa1b99033b178
Hah-nna <hj08hn@gmail.com>
2025-02-05 21:11:45 +0900

    fix: font family
    - 폰트 패밀리를 추가합니다
    - weight를 수정합니다
    
    by Hah-nna

--- app/src/main/java/com/jeong/sesac/chaksaicompose/ui/theme/Type.kt
+++ app/src/main/java/com/jeong/sesac/chaksaicompose/ui/theme/Type.kt
@@ -11,59 +11,62 @@ import com.jeong.sesac.chaksaicompose.R
 const val lineHeightPercentage = 1.5f
 
 val pretendardFontFamily = FontFamily(
-    Font(R.font.pretendard, FontWeight.Normal)
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_semi_bold, FontWeight.SemiBold),
+    Font(R.font.pretendard_bold, FontWeight.Bold),
+    Font(R.font.pretendard_extra_bold, FontWeight.ExtraBold)
 )
 
 val AppTypography = Typography(
     titleLarge = TextStyle(
         fontFamily = pretendardFontFamily,
-        fontWeight = FontWeight.ExtraBold,
+        fontWeight = FontWeight.W600,
         fontSize = 22.sp,
         lineHeight = 24.sp * lineHeightPercentage
     ),
     titleMedium = TextStyle(
         fontFamily = pretendardFontFamily,
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.W600,
         fontSize = 18.sp,
         lineHeight = 18.sp * lineHeightPercentage
     ),
     titleSmall = TextStyle(
         fontFamily = pretendardFontFamily,
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.W600,
         fontSize = 14.sp,
         lineHeight = 14.sp * lineHeightPercentage
     ),
     // button
     labelLarge = TextStyle(
         fontFamily = pretendardFontFamily,
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.W500,
         fontSize = 18.sp,
     ),
     labelMedium = TextStyle(
         fontFamily = pretendardFontFamily,
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.W500,
         fontSize = 16.sp,
     ),
     labelSmall = TextStyle(
         fontFamily = pretendardFontFamily,
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.W500,
         fontSize = 14.sp,
     ),
     // body
     bodyLarge = TextStyle(
         fontFamily = pretendardFontFamily,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.W400,
         fontSize = 18.sp,
         lineHeight = 18.sp * lineHeightPercentage
     ),
     bodyMedium = TextStyle(
         fontFamily = pretendardFontFamily,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.W400,
         fontSize = 14.sp,
     ),
     bodySmall = TextStyle(
         fontFamily = pretendardFontFamily,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.W400,
         fontSize = 10.sp,
     ),
 )
