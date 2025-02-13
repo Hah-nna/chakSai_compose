a4ba77850bcd538fad4a150277aca46de32e6f3e
Hah-nna <hj08hn@gmail.com>
2025-02-14 00:05:24 +0900

    fix: app typography 추가함 by Hah-nna

---
+++ app/src/main/java/com/jeong/sesac/chaksaicompose/ui/theme/AppTypography.kt
@@ -0,0 +1,13 @@
+package com.jeong.sesac.chaksaicompose.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.jeong.sesac.chaksaicompose.R
+
+val pretendardFontFamily = FontFamily(
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_semi_bold, FontWeight.SemiBold),
+    Font(R.font.pretendard_bold, FontWeight.Bold),
+    Font(R.font.pretendard_extra_bold, FontWeight.ExtraBold)
+)
