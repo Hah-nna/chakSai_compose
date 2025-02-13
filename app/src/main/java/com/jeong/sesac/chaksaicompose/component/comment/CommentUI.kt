425e5771e1e238f77ca3a63427a114ae8b038c74
Hah-nna <hj08hn@gmail.com>
2025-02-14 05:04:01 +0900

    component: commentUI - 코멘트UI를 컴포넌트로 분리합니다 by Hah-nna

---
+++ app/src/main/java/com/jeong/sesac/chaksaicompose/component/comment/CommentUI.kt
@@ -0,0 +1,35 @@
+package com.jeong.sesac.chaksaicompose.component.comment
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.jeong.sesac.chaksaicompose.component.ProfileSmall
+import com.jeong.sesac.chaksaicompose.ui.theme.AppTheme
+import com.jeong.sesac.feature.model.Comment
+
+@Composable
+fun CommentUI(itemData: Comment, img: Int) {
+    Column(
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.padding(vertical = AppTheme.size.small)
+    ) {
+        Row (
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            ProfileSmall(img)
+            Text(itemData.userInfo.nickName, modifier = Modifier.padding(horizontal = AppTheme.size.small))
+            Text(itemData.createdAt, style = AppTheme.typography.labelSmall, color = Color.DarkGray)
+        }
+        Text(itemData.content, modifier = Modifier.padding(vertical = AppTheme.size.small))
+        HorizontalDivider()
+    }
+}
