a3797b26f87ffe13efc74efeea3f721f06db9650
Hah-nna <hj08hn@gmail.com>
2025-02-14 00:06:27 +0900

    feat: custom theme 추가함 by Hah-nna

--- app/src/main/java/com/jeong/sesac/chaksaicompose/ui/theme/Theme.kt
+++ app/src/main/java/com/jeong/sesac/chaksaicompose/ui/theme/Theme.kt
@@ -1,17 +1,17 @@
 package com.jeong.sesac.chaksaicompose.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-private val DarkColorScheme = darkColorScheme(
+const val lineHeightPercentage = 1.5f
+
+private val DarkColorScheme = AppColorScheme(
     primary = YellowDark100,
     secondary = YellowDark200,
     tertiary = GreenDark100,
@@ -29,7 +29,7 @@ private val DarkColorScheme = darkColorScheme(
 
 )
 
-private val LightColorScheme = lightColorScheme(
+private val LightColorScheme = AppColorScheme(
     primary = Yellow300,
     secondary = Yellow400,
     tertiary = Green100,
@@ -44,29 +44,101 @@ private val LightColorScheme = lightColorScheme(
     primaryContainer = Yellow500,
     secondaryContainer =Yellow600,
     tertiaryContainer = Green200
+)
 
+private val typography = AppTypography(
+    titleLarge = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 22.sp,
+        lineHeight = 24.sp * lineHeightPercentage
+    ),
+    titleMedium = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 18.sp,
+        lineHeight = 18.sp * lineHeightPercentage
+    ),
+    titleSmall = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 14.sp,
+        lineHeight = 14.sp * lineHeightPercentage
+    ),
+    // button
+    labelLarge = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 18.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 16.sp,
+    ),
+    labelSmall = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp,
+    ),
+    // body
+    bodyLarge = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 18.sp,
+        lineHeight = 18.sp * lineHeightPercentage
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 10.sp,
+    ),
+)
+
+private val shape = AppShape(
+    container = RoundedCornerShape(12.dp),
+    button = RoundedCornerShape(8.dp)
+)
+
+private val size = AppSize(
+    large = 24.dp,
+    medium = 16.dp,
+    normal = 12.dp,
+    small = 8.dp
 )
 
 @Composable
-fun ChakSaiComposeTheme(
+fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = AppTypography,
+    val colorScheme = if(darkTheme) DarkColorScheme else LightColorScheme
+    CompositionLocalProvider(
+        LocalAppColorScheme provides colorScheme,
+        LocalAppTypography provides typography,
+        LocalAppShape provides shape,
+        LocalAppSize provides size,
         content = content
     )
+
 }
+
+object AppTheme {
+    val colorScheme: AppColorScheme
+        @Composable get() = LocalAppColorScheme.current
+
+    val typography: AppTypography
+        @Composable get() = LocalAppTypography.current
+
+    val shape: AppShape
+        @Composable get() = LocalAppShape.current
+
+    val size: AppSize
+        @Composable get() = LocalAppSize.current
+}
+
