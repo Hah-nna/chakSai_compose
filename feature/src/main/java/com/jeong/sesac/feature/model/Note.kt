1b22b76c2b6c9e04638671ecdc2360f90aeb281a
Hah-nna <hj08hn@gmail.com>
2025-03-07 01:13:30 +0900

    fix: edit models by Hah-nna

--- feature/src/main/java/com/jeong/sesac/feature/model/Note.kt
+++ feature/src/main/java/com/jeong/sesac/feature/model/Note.kt
@@ -1,38 +1,70 @@
 package com.jeong.sesac.feature.model
 
-import java.util.Date
 
-data class UserInfo(
+data class PlaceInfo(
+    val id: String,
+    val address: String,
+    val phone: String,
+    val place: String,
+    val placeURL: String,
+    val lat: String,
+    val lng: String,
+    val distance: String,
+    val searchRadius: Int
+)
+
+data class Note(
+    val id: String = "",
+    val userId: String = "",
+    val image: String = "",
+    val title: String = "",
+    val createdAt: Long = System.currentTimeMillis(),
+    val libraryName: String = "",
+    val content: String = "",
+    val likes: List<String> = emptyList(),
+)
+
+data class NoteWithUser(
+    val id: String = "",
+    val userInfo: UserInfo = UserInfo("", "", ""),
+    val image: String = "",
+    val title: String = "",
+    val createdAt: Long = 0L,
+    val libraryName: String = "",
+    val content: String = "",
+    val likes: List<String> = emptyList(),
+)
+
+data class UserInfo (
     val id: String,
     val profile: String,
     val nickName: String
 )
 
-data class NoteWithUser(
-    val id: String,
-    val userInfo: UserInfo,
-    val image: Int,
-    val title: String,
-    val createdAt: Date,
-    val libraryName: String,
-    val content: String,
-    val likes: Int,
+data class Comment(
+    val id: String = "",
+    val userId: String = "",
+    val noteId: String = "",
+    val content: String ="",
+    val createdAt: Long = System.currentTimeMillis()
 )
 
-data class Comment(
-    val id: String,
-    val userId: String,
-    val noteId: String,
-    val content: String,
-    val createdAt: String,
-    val userInfo: UserInfo
+data class CommentWithUser(
+    val id: String = "",
+    val userInfo: UserInfo = UserInfo(
+        id = "",
+        profile = "",
+        nickName = ""
+    ),
+    val content: String = "",
+    val createdAt: Long = 0L
 )
 
 data class User(
     val id: String,
     val profile: String,
     val nickname: String,
-    val created_at: String,
+    val created_at: Long,
     val provider_info: ProviderType = ProviderType.KAKAO
 ) {
     fun toMap(): Map<String, Any> {
