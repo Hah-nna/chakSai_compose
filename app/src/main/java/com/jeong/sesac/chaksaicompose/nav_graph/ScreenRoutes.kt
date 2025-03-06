f1a8c96632879dd5ddee0379865cfd655e40006c
Hah-nna <hj08hn@gmail.com>
2025-03-07 01:10:03 +0900

    feat: add screens and separate post detail by Hah-nna

--- app/src/main/java/com/jeong/sesac/chaksaicompose/nav_graph/ScreenRoutes.kt
+++ app/src/main/java/com/jeong/sesac/chaksaicompose/nav_graph/ScreenRoutes.kt
@@ -2,7 +2,7 @@ package com.jeong.sesac.chaksaicompose.nav_graph
 
 sealed class ScreenRoutes(val routeName : String) {
     /**
-     * bototm tab routes
+     * bottom tab routes
      * */
     data object HomeTab : ScreenRoutes("Home")
     data object LibraryMapTab : ScreenRoutes("Map")
@@ -13,26 +13,42 @@ sealed class ScreenRoutes(val routeName : String) {
      * 하위 스크린
      * */
     sealed interface HomeTabScreenGroup {
-        // 이번주 새로운 쪽지 리스트 스크린
-        data object NewNotes : ScreenRoutes("new_notes")
-        // 이번 주 인기 쪽지 리스트 스크린
-        data object WeeklyNotes : ScreenRoutes("weekly_notes")
+        // 이번주 새로운 post 리스트 스크린
+        data object NewNotes : ScreenRoutes("new_posts")
+        // 이번 주 인기 post 리스트 스크린
+        data object WeeklyNotes : ScreenRoutes("weekly_posts")
     }
 
     sealed interface LibraryMapTabScreenGroup {
-        // 도서관을 선택하고 해당 도서관에 쪽지를 작성하는 스크린
-        data object LibraryWriteNote : ScreenRoutes("library_write_note")
-        // 내가 선택한 도서관 및 다른 도서관에 작성된 쪽지들 리스트들을 볼 수 있는 스크린
-        data object LibraryNoteList : ScreenRoutes("library_note_list")
-
+        // 도서관에 작성된 post 리스트들을 볼 수 있는 스크린
+        data object LibraryPostList : ScreenRoutes("library_post_list")
+        // 해당 도서관에 post를 작성하는 스크린
+        data object LibraryWritePost : ScreenRoutes("library_write_post")
+        // 해당 도서관에 등록된 북 리스트 스크린
+        data object LibraryBook : ScreenRoutes("library_book_list")
+        // 북 디테일 페이지
+        data object LibraryBookDetail : ScreenRoutes("library_book_detail")
+        // 북+서평 등록 페이지
+        data object LibraryWriteBook : ScreenRoutes("library_write_book")
+        // 서평 수정 페이지
+        data object LibraryEditBookReview : ScreenRoutes("edit_bookReview")
     }
 
     sealed interface MyPageScreenGroup {
+        // 메인 마이페이지 스크린
         data object MyProfile : ScreenRoutes("my_profile")
+        // 닉네임, 프로필 수정 페이지
+        data object EditMyInfo : ScreenRoutes("edit_myInfo")
+        // 문의 페이지
+        data object Contact: ScreenRoutes("contact")
     }
 
-    sealed interface CommonScreenGroup {
-        // 쪽지 디테일 스크린
-        data object LibraryNoteDetail : ScreenRoutes("library_note_detail")
+    sealed interface PostDetailScreenGroup {
+        // 포스트 디테일 스크린
+        data object LibraryPostDetail : ScreenRoutes("library_post_detail")
+        // 포스트 수정 스크린
+        data object EditPostScreen : ScreenRoutes("edit_post")
+        // comment 수정 스크린
+        data object EditCommentScreen : ScreenRoutes("edit_comment")
     }
 }
