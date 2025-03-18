a369e365e69620c613c3f90ded1c3fa87835ce67
Hah-nna <hj08hn@gmail.com>
2025-03-18 21:04:10 +0900

    fix: 상태관리 추가 by Hah-nna

--- app/src/main/java/com/jeong/sesac/chaksaicompose/ui/home/HomeTabScreen.kt
+++ app/src/main/java/com/jeong/sesac/chaksaicompose/ui/home/HomeTabScreen.kt
@@ -1,52 +1,70 @@
 package com.jeong.sesac.chaksaicompose.ui.home
 
-import android.widget.Toast
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.jeong.sesac.chaksaicompose.R
-import com.jeong.sesac.chaksaicompose.common.mockNotes
+import com.jeong.sesac.chaksaicompose.common.AppPreferenceManager
 import com.jeong.sesac.chaksaicompose.component.CommonSpacer
 import com.jeong.sesac.chaksaicompose.component.home.LazyColUI
 import com.jeong.sesac.chaksaicompose.component.home.TitleUi
-import com.jeong.sesac.chaksaicompose.nav_graph.ScreenRoutes
+import com.jeong.sesac.chaksaicompose.model.UiState
 import com.jeong.sesac.chaksaicompose.ui.theme.AppTheme
-import com.jeong.sesac.feature.model.NoteWithUser
+import com.jeong.sesac.chaksaicompose.viewModel.PostListViewModel
+import com.jeong.sesac.chaksaicompose.viewModel.appViewModelFactory
+import com.jeong.sesac.feature.model.PostWithUser
 
 
 @Composable
 fun HomeTabScreen(
-    navController: NavController
+    preference: AppPreferenceManager,
+    onNavigationUp: () -> Unit,
+    onNavigationToNewPostList: (posts: List<PostWithUser>) -> Unit,
+    onNavigationToPopularNotes: (posts: List<PostWithUser>) -> Unit,
+    onNavigationToDetailPost: (postId: String) -> Unit,
+    viewModel: PostListViewModel = viewModel(factory = appViewModelFactory)
 ) {
-    HomeTabContent(navController)
+    HomeTabContent(
+        preference,
+        onNavigationUp,
+        onNavigationToNewPostList,
+        onNavigationToPopularNotes,
+        onNavigationToDetailPost,
+        viewModel
+    )
 }
 
 
 @Composable
-private fun HomeTabContent(navController : NavController) {
-    val mockData = mockNotes
-    // post 디테일 페이지 이동 nav 함수
-    val onPostDetailClick = { noteData: NoteWithUser ->
-        navController.navigate(ScreenRoutes.PostDetailScreenGroup.LibraryPostDetail.routeName + "/${noteData.id}")
+private fun HomeTabContent(
+    preference: AppPreferenceManager,
+    onNavigationUp: () -> Unit,
+    onNavigationToNewPostList: (postData: List<PostWithUser>) -> Unit,
+    onNavigationToPopularNotes: (postData: List<PostWithUser>) -> Unit,
+    onNavigationToDetailPost: (postId: String) -> Unit,
+    viewModel: PostListViewModel = viewModel(factory = appViewModelFactory)
+) {
+    val nickname = preference.nickName
+    val userId = preference.userId
+
+    LaunchedEffect(Unit) {
+        viewModel.getNewPostList(userId)
+        viewModel.getPopularNoteList(userId)
     }
-    // 이번 주 인기 post 리스트 페이지 이동 함수
-    val onWeeklyPostsClick = {
-        navController.navigate(ScreenRoutes.HomeTabScreenGroup.WeeklyPosts)
-    }
-    // 이번 주 새로 등록된 post 리스트 페이지 이동 함수
-    val onNewPostsClick = {
-        navController.navigate(ScreenRoutes.HomeTabScreenGroup.NewPosts)
-    }
+
+    val popularPostsState by viewModel.popularPostsState.collectAsStateWithLifecycle()
+    val newPostsState by viewModel.newPostsState.collectAsStateWithLifecycle()
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Top,
@@ -56,21 +74,49 @@ private fun HomeTabContent(navController : NavController) {
         CommonSpacer(24)
 
         Text(
-            stringResource(R.string.main_header, "하겐다즈"),
+            stringResource(R.string.main_header, nickname),
             style = AppTheme.typography.titleLarge
         )
         CommonSpacer(24)
 
-        TitleUi(R.string.main_weekly_popular_note_title, onMoreClick = onWeeklyPostsClick)
+        TitleUi(R.string.main_weekly_popular_note_title, onMoreClick = {
+            (popularPostsState as UiState.Success).data.let { posts ->
+                onNavigationToPopularNotes(posts)
+            }
+        })
+        when (val popularPosts = popularPostsState) {
+            is UiState.Loading -> {
+            }
 
-        LazyColUI(mockData, onPostClick = onPostDetailClick)
+            is UiState.Success -> {
+                LazyColUI(popularPosts.data, onPostClick = { onNavigationToDetailPost(it.id) })
+
+            }
+
+            is UiState.Error -> {
+                Text("err : ${popularPosts.error}")
+            }
+        }
 
         CommonSpacer(24)
 
-        TitleUi(R.string.main_recently_new_note_title, onMoreClick = onNewPostsClick)
+        TitleUi(R.string.main_recently_new_note_title, onMoreClick = {
+            (newPostsState as UiState.Success).data.let { newPosts -> onNavigationToNewPostList(newPosts) }
+        })
 
-        LazyColUI(mockData, onPostClick = onPostDetailClick)
+        when (val newPosts = newPostsState) {
+            is UiState.Loading -> {
+            }
 
+            is UiState.Success -> {
+                LazyColUI(newPosts.data, onPostClick = { onNavigationToDetailPost(it.id) })
+
+            }
+
+            is UiState.Error -> {
+                Text("err : ${newPosts.error}")
+            }
+        }
     }
 
 }
@@ -79,8 +125,7 @@ private fun HomeTabContent(navController : NavController) {
 @Preview(showBackground = true)
 @Composable
 private fun HomeTabScreenPreview() {
-    val previewNavController = rememberNavController()
-    AppTheme{
-    HomeTabScreen(navController = previewNavController)
+    AppTheme {
+//        HomeTabScreen()
     }
 }
