90050eec2aa27cd26117c4774584736062ecd9fd
Hah-nna <hj08hn@gmail.com>
2025-03-18 21:28:01 +0900

    feat: ProfileSmall component 추가
    by Hah-nna

--- app/src/main/java/com/jeong/sesac/chaksaicompose/component/img/ProfileImg.kt
+++ app/src/main/java/com/jeong/sesac/chaksaicompose/component/img/ProfileImg.kt
@@ -1,6 +1,5 @@
-package com.jeong.sesac.chaksaicompose.component
+package com.jeong.sesac.chaksaicompose.component.img
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -9,18 +8,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import coil3.compose.AsyncImage
 import com.jeong.sesac.chaksaicompose.R
+import com.jeong.sesac.chaksaicompose.common.coil.CoilImgRequest
 
 @Composable
-fun ProfileSmall(img: Int) {
-    Image(
-        painter = painterResource(img),
-        contentDescription = "profile_img",
-        contentScale = ContentScale.FillBounds,
+fun ProfileSmall(img: String) {
+    AsyncImage(
         modifier = Modifier
             .width(dimensionResource(R.dimen.profileSmall))
             .height(dimensionResource(R.dimen.profileSmall))
-            .clip(CircleShape)
+            .clip(CircleShape),
+        model = CoilImgRequest.getImgRequest(img),
+        contentScale = ContentScale.FillBounds,
+        contentDescription = stringResource(R.string.profileImage),
+        onState = { state -> CoilImgRequest.coilCallbackLog(img, state) }
     )
 }
