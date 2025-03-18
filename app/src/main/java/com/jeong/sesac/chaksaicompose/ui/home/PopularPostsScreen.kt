16b709a0952cbdc77f38b443838613b7ad39e70a
Hah-nna <hj08hn@gmail.com>
2025-03-18 21:26:03 +0900

    fix: 이름 변경, 하드코딩 -> stringResource 수정, 파라미터 수정
    by Hah-nna

--- app/src/main/java/com/jeong/sesac/chaksaicompose/ui/home/PopularPostsScreen.kt
+++ app/src/main/java/com/jeong/sesac/chaksaicompose/ui/home/PopularPostsScreen.kt
@@ -9,11 +9,12 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
+import com.jeong.sesac.chaksaicompose.R
+import com.jeong.sesac.chaksaicompose.common.AppPreferenceManager
 import com.jeong.sesac.chaksaicompose.common.mockNotes
-import com.jeong.sesac.chaksaicompose.component.CommonTopAppBar
+import com.jeong.sesac.chaksaicompose.component.BasicTopAppBar
 import com.jeong.sesac.chaksaicompose.component.tab.GridNoteList
 import com.jeong.sesac.chaksaicompose.component.tab.TabContentUI
 import com.jeong.sesac.chaksaicompose.component.tab.TabLayoutUI
@@ -22,16 +23,18 @@ import com.jeong.sesac.chaksaicompose.ui.theme.AppTheme
 private fun tabItemList() = listOf("최신순", "좋아요높은순", "좋아요낮은순")
 
 @Composable
-fun WeeklyPostsScreen (
-    navController : NavController
+fun PopularPostsScreen (
+    preference: AppPreferenceManager,
+    onNavigationUp: () -> Unit,
+    onNavigationToDetailPost: (postId: String) -> Unit
 ) {
-    CommonTopAppBar("최근 등록된 쪽지", "go to back") { padding ->
-        WeeklyPostsContent(tabItemList(), padding)
+    BasicTopAppBar(stringResource(R.string.main_weekly_popular_note_title), stringResource(R.string.goToBack), onNavigationUp) { padding ->
+        PopularPostsContent(tabItemList(), padding, onNavigationToDetailPost)
     }
 }
 
 @Composable
-fun WeeklyPostsContent(tabItemList: List<String>, innerPadding: PaddingValues) {
+fun PopularPostsContent(tabItemList: List<String>, innerPadding: PaddingValues, onNavigationToDetailPost: (postId: String) -> Unit) {
     val pagerState = rememberPagerState(initialPage = 0) { tabItemList.size }
 
     Column(
@@ -49,9 +52,9 @@ fun WeeklyPostsContent(tabItemList: List<String>, innerPadding: PaddingValues) {
         TabContentUI(
             pagerState = pagerState,
             pages = listOf(
-                { GridNoteList(mockNotes) },
-                { GridNoteList(mockNotes) },
-                { GridNoteList(mockNotes) }
+                { GridNoteList(mockNotes, onNavigationToDetailPost) },
+                { GridNoteList(mockNotes, onNavigationToDetailPost) },
+                { GridNoteList(mockNotes, onNavigationToDetailPost) }
             )
         )
     }
@@ -60,9 +63,8 @@ fun WeeklyPostsContent(tabItemList: List<String>, innerPadding: PaddingValues) {
 
 @Preview(showBackground = true)
 @Composable
-private fun WeeklyNotesScreenPreview() {
-    val previewNavController = rememberNavController()
+private fun PopularNotesScreenPreview() {
     AppTheme {
-        WeeklyPostsScreen(navController = previewNavController)
+//        PopularPostsScreen()
     }
 }
