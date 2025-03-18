0e2510013c34d69913461bfe3652e88d395b211c
Hah-nna <hj08hn@gmail.com>
2025-03-18 21:08:03 +0900

    fix: 포스트 디테일 네비게이션 그래프 추가 by Hah-nna

--- app/src/main/java/com/jeong/sesac/chaksaicompose/nav_graph/PostDetailNavGraph.kt
+++ app/src/main/java/com/jeong/sesac/chaksaicompose/nav_graph/PostDetailNavGraph.kt
@@ -1,86 +1,80 @@
 package com.jeong.sesac.chaksaicompose.nav_graph
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
-import androidx.navigation.navArgument
-import com.jeong.sesac.chaksaicompose.R
-import com.jeong.sesac.chaksaicompose.ui.postDetail.PostDetailScreen
-import com.jeong.sesac.feature.model.NoteWithUser
-import com.jeong.sesac.feature.model.UserInfo
+import com.jeong.sesac.chaksaicompose.ui.post_detail.PostDetailScreen
+import kotlinx.serialization.Serializable
 
+@Serializable
+data object PostDetailRoute
 
-fun NavGraphBuilder.PostDetailNavGraph(navController: NavController) {
-    navigation(
-        startDestination = ScreenRoutes.PostDetailScreenGroup.LibraryPostDetail.routeName,
-        route = "post_detail_nav_graph"
+@Serializable
+data class PostDetail(
+    val postId: String
+)
+
+@Serializable
+data class PostEdit(
+    val postId: String
+)
+
+@Serializable
+data class CommentEdit(
+    val commentId: String
+)
+
+/**
+ * 포스트 디테일 그래프
+ * 포스트 수정, 코멘트 수정 스크린도 포함
+ * */
+fun NavGraphBuilder.postDetailNavGraph(
+    onNavigationUp: () -> Unit,
+    onNavigationToEditPost: (postId: String) -> Unit,
+    onNavigationToEditComment: (commentId: String) -> Unit
+) {
+    navigation<PostDetailRoute>(
+        startDestination = PostDetail::class
+//        route = "post_detail_nav_graph"
     ) {
-        composable(
-            route = ScreenRoutes.PostDetailScreenGroup.LibraryPostDetail.routeName + "/{postId}",
-            arguments = listOf(
-                navArgument("postId") {
-                    type = NavType.StringType
-                    nullable = false
-                }
-            )
-        ) { backStackEntry ->
+        composable<PostDetail> { backStackEntry ->
             val postId = backStackEntry.arguments?.getString("postId")
                 ?: return@composable
 
             PostDetailScreen(
-                noteId = postId,
-                note = tempNoteDate(),
-                onBackClick = { navController.popBackStack() }
+                postId = postId,
+                onBackClick = onNavigationUp,
+                onEditPostClick = { onNavigationToEditPost(postId) },
+                onEditCommentClick = { commentId -> onNavigationToEditComment(commentId) }
             )
         }
-        composable(
-            route = ScreenRoutes.PostDetailScreenGroup.EditPostScreen.routeName + "/{postId}",
-            arguments = listOf(
-                navArgument("postId") {
-                    type = NavType.StringType
-                    nullable = false
-                }
-            )
-        ) {
-            backStackEntry ->
+        composable<PostEdit> { backStackEntry ->
             val postId = backStackEntry.arguments?.getString("postId")
                 ?: return@composable
 
-//            EditPostScreen()
+//            EditPostScreen(
+//                postId = postId,
+//                onBackClick = onNavigationUp,
+//            )
         }
-        composable(
-            route = ScreenRoutes.PostDetailScreenGroup.EditCommentScreen.routeName + "/{commentId}",
-            arguments = listOf(
-                navArgument("commentId") {
-                    type = NavType.StringType
-                    nullable = false
-                }
-            )
-        ) { backStackEntry ->
+
+        composable<CommentEdit>
+//        (
+//            route = ScreenRoutes.PostDetailScreenGroup.EditCommentScreen.routeName + "/{commentId}",
+//            arguments = listOf(
+//                navArgument("commentId") {
+//                    type = NavType.StringType
+//                    nullable = false
+//                }
+//            )
+//        )
+        { backStackEntry ->
             val commentId = backStackEntry.arguments?.getString("commentId")
                 ?: return@composable
-//            EditCommentScreen
+//            EditCommentScreen(
+//                commentId = commentId,
+//                onBackClick = onNavigationUp,
+//            )
         }
     }
-
-
-
-}
-private fun tempNoteDate(): NoteWithUser {
-    return NoteWithUser(
-        id = "note3",
-        userInfo = UserInfo(
-            id = "user456",
-            profile = "https://example.com/profile.jpg",
-            nickName = "벤앤제리"
-        ),
-        image = R.drawable.ic_launcher_background.toString(),
-        title = "세 번째 쪽지",
-        createdAt = System.currentTimeMillis(),
-        libraryName = "구의도서관",
-        content = "새로 나온 책 추천합니다",
-        likes = emptyList()
-    )
 }
