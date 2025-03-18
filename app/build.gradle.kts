c43bda44ceb39efa47d5709233afc87b881d301e
Hah-nna <hj08hn@gmail.com>
2025-03-18 20:35:46 +0900

    chore: serialize 관련 디펜던시 추가 by Hah-nna

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services)
     id("kotlin-kapt")
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 // local.properties 안의 내용 읽기
@@ -136,6 +138,9 @@ dependencies {
 
     // MLKit barcode
     implementation(libs.barcode.scanning)
+
+    // serialize
+    implementation(libs.kotlinx.serialization.json)
 
 
 }
